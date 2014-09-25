bd538c6e25a0d7dfb0cf1e87f59478b37469c5a3
suntootoo <suntootoo@gmail.com>
2014-09-26 02:07:22 +0800

    Finish Sign_in & Sign_out (although I don't really know about the session...O.O...)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_create  :create_remember_token
 	before_save { self.email = email.downcase }
 	validates :name,presence: true,length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +9,16 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password,length:  { minimum: 6 }
 	validates :password_confirmation,presence: true
+
+	def User.generate_token
+		SecureRandom.urlsafe_base64
+	end
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+	private
+		def create_remember_token
+			self.remember_token = User.hash(User.generate_token)
+		end
+	
 end
